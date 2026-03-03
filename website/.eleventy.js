07b639678e33bae2a3d94d8e7276fd27f9399cd7
1u <4wangyu@gmail.com>
2026-03-03 11:10:47 +0800

    refactor: remove `welcome-card` and `stat` classes from `index.njk` and stop copying `src/js` assets.

--- website/.eleventy.js
+++ website/.eleventy.js
@@ -3,7 +3,6 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(syntaxHighlight);
     eleventyConfig.addPassthroughCopy("src/css");
-    eleventyConfig.addPassthroughCopy("src/js");
     eleventyConfig.addPassthroughCopy("src/problems");
     eleventyConfig.addPassthroughCopy("src/.nojekyll");
 
